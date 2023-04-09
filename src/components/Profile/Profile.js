7fe1bc7d2265f04bb56532babb1c5850faee42ac
YuliiaKostishyna <112808612+YuliiaKostishyna@users.noreply.github.com>
2023-04-09 21:31:40 +0300

    Update Profile.js

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -5,8 +5,8 @@ const Profile = ({ username, tag, location, avatar, stats }) => (
   <div className="profile">
     <div className="description">
       <img src={avatar} alt="User avatar" className="avatar" />
-      <p className="name">@{username}</p>
-      <p className="tag">{tag}</p>
+      <p className="name">{username}</p>
+      <p className="tag">@{tag}</p>
       <p className="location">{location}</p>
     </div>
 
